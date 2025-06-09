847c3807678b640498588dbd596503ea93f0437e
dawn <chimerezedawn6@gmail.com>
2025-06-09 15:28:59 +0100

    fix i hope

--- next.config.js
+++ next.config.js
@@ -12,6 +12,14 @@ const nextConfig = {
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
   },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      bufferutil: false,
+      'utf-8-validate': false,
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
